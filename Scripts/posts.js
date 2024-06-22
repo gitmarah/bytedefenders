5154ff1f539caec867a3620b48d688b737f1826e
gitmarah <telastudio2023@gmail.com>
2024-06-22 11:01:42 +0000

    Post Update

--- Scripts/posts.js
+++ Scripts/posts.js
@@ -1,7 +1,15 @@
 const posts = [
     {
+        header: "SIERRA LEONE CYBER SECURITY AND CRIME ACT 2021 - OFFENCES",
+        shownText: "<b>UNAUTHORISED ACCESS 33 (1):</b><br>👉A person, including a corporation, partnership, or association, who intentionally and without authorisation causes a computer system to perform a function with intent to ",
+        moreText: `secure access to the whole or a part of a computer system or to enable such access to be secured other than to secure and protect the integrity of digital communications or for unlawful purposes, commits an offence and is liable upon conviction to fine not less than NLe100,000 and not more than NLe 250,000 or to a term of imprisonment not less than 2 years and not exceeding 5 years or to both such fine and imprisonment.<br><br>
+            👉And in the case of a corporation, partnership, or association, to a fine not less than NLe500,000 and not exceeding NLe1,000,000.✍️
+        `,
+        image: "assets/Post Image/postImg3.jpg",
+    },
+    {
         header: "HOW TO IDENTIFY PHISHING EMAILS AND MESSAGES - REQUESTS FOR PASSWORD",
-        shownText: "<b>Indicator:</b>Legitimate organizations typically won't ask for sensitive information (like passwords) via email.",
+        shownText: "<b>Indicator:</b> Legitimate organizations typically won't ask for sensitive information (like passwords) via email.",
         moreText: `<br><b>Example:</b>
             An email asking you to "confirm your social security number" or "provide your password" in a form.✍️
             <br><br><b>Key:</b>If you are asked to fill a form by giving your password, it means they are bad guys. Your email is known publicly, all they need is your password, so they use forms to easily get passwords. Never❌ fill a form by giving out your password.✍️

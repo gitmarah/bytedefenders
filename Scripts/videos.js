d1a340e77d2cd3aafb3bdff6f5cd3e9960ee1642
gitmarah <telastudio2023@gmail.com>
2024-07-01 22:20:53 +0000

    Event Update

--- Scripts/videos.js
+++ Scripts/videos.js
@@ -6,7 +6,7 @@ const videos = [
     },
     {
         videoTitle: "LEGAL KNOWLEDGE - Episode 1 | ByteDefenders",
-        videoDescription: "Watch this video to gain more insight on Cyber Security.",
+        videoDescription: "Watch this video to gain more insight on Cyber Laws.",
         videoLink: `<iframe width="560" src="https://www.youtube.com/embed/EdthNI4Lz0Q?si=rNMVwC__JrnhTSsq" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>`
     },
     {
@@ -14,11 +14,11 @@ const videos = [
         videoDescription: "Watch this video to gain more insight on Cyber Security.",
         videoLink: `<iframe width="560" src="https://www.youtube.com/embed/inWWhr5tnEA?si=3TBkrTkVJdcV9iPl" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>`
     },
-    {
-        videoTitle: "Google Authenticator App | 2-Factor Authentication",
-        videoDescription: "Watch this video to learn how to use the app the syncs one-time passwords (OTP) with your google account!",
-        videoLink: `<iframe width="560" src="https://www.youtube.com/embed/3eTify98puI?si=JdAD89zhZQ8mEvro" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>`,
-    },
+    // {
+    //     videoTitle: "Google Authenticator App | 2-Factor Authentication",
+    //     videoDescription: "Watch this video to learn how to use the app the syncs one-time passwords (OTP) with your google account!",
+    //     videoLink: `<iframe width="560" src="https://www.youtube.com/embed/3eTify98puI?si=JdAD89zhZQ8mEvro" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>`,
+    // },
     {
         videoTitle: "What is a FireWall? | Simplilearn",
         videoDescription: "Watch this video to gain more insight on FireWalls.",

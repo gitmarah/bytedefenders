7c7b9243a7508fd3b3e5bf3b4da0de936b143c37
gitmarah <telastudio2023@gmail.com>
2024-07-01 21:58:23 +0000

    Event Update

--- Scripts/events.js
+++ Scripts/events.js
@@ -1,4 +1,4 @@
-
+import { eventPosts } from "./event-posts.js";
 //OFFCANVAS MENU
 const offcanvasIcon = document.querySelector(".sm-screen-nav");
 const offcanvas = document.querySelector(".offcanvas");
@@ -30,6 +30,41 @@ offcanvasIcon.addEventListener('click', () => {
     }
 });
 
-
+eventPosts.forEach((post) => {
+    document.querySelector(".event-posts").innerHTML += `
+    <div class="event-post-div">
+        <div class="post">
+                <h2>${post.postHead}</h2>
+                <p class="post-text js-post-text">${post.shownContent}
+                <span class="read-more-text">
+                    ${post.moreContent}
+                </span>
+                <span class="read-more-link js-read-more-link">...see more</span>
+                </p>   
+            </div>
+            <div class="img-div">
+                <img src="${post.img1}" alt="Meeting Picture with Mr Ibrahim Kalokoh">
+                <img src="${post.img2}" alt="Meeting Picture with Mr Ibrahim Kalokoh">
+            </div>
+            <p class="date">${post.date}</p>
+            </div>
+    `;
+    const postContents = document.querySelectorAll('.js-post-text');
+    const seeMoreBtns = document.querySelectorAll('.js-read-more-link');
+    const seeMoreText = document.querySelectorAll(".read-more-text");   
+    postContents.forEach((postContent, index) => {
+        postContent.addEventListener('click', () => {
+            if(!postContent.dataset.clicked){
+                postContent.setAttribute("data-clicked", "true");
+                seeMoreText[index].style.display = "inline";
+                seeMoreBtns[index].innerHTML = "collapse";
+            }else{
+                postContent.removeAttribute("data-clicked");
+                seeMoreText[index].style.display = "none";
+                seeMoreBtns[index].innerHTML = "...see more";
+            }
+        });
+    });
+});
 
 
